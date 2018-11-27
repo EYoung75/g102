e15eaf5272a6f9148b609defc2673d3396681f8d
EYoung75 <eyoung75.ey@gmail.com>
2018-11-27 12:58:33 -0700

    remove gitignore files

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const queries = require('./queries')
 
-app.listen(port, () => console.log(`Listening on port ${port}!`))
+app.listen(port)
 
 app.get("/", (req, res) => {
     queries.listAll().then(students => res.send(students))
