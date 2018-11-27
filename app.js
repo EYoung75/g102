991dea1c6f50baa8792a0562e51ca99545179817
EYoung75 <eyoung75.ey@gmail.com>
2018-11-27 11:59:24 -0700

    wrote and ran queries for database of student information

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const queries = require('./queries')
 
 app.listen(port, () => console.log(`Listening on port ${port}!`))
 
 app.get("/", (req, res) => {
-    res.send("Hola bitchez")
-    console.log(knex.schema)
+    queries.listAll().then(students => res.send(students))
 })
