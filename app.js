e41c8782db0f64f7253589a7ad9bab442f274b73
EYoung75 <eyoung75.ey@gmail.com>
2018-11-28 10:22:12 -0700

    wrote queries to select by id and post route

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
-const queries = require('./queries')
+const queries = require('./queries');
+
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+app.use(bodyParser.json());
+app.use(cors());
 
 app.listen(port)
 
 app.get("/", (req, res) => {
-    queries.listAll().then(students => res.send(students))
+    queries.listAll().then(students => res.send(students[0]))
 })
+
+app.post("/", (req, res) => {
+    queries.createStudent(req.body).then(students => res.send(students))
+})
+
+app.get("/:id", (req, res) => {
+    queries.getById(req.params.id).then(student => res.send(student[0]))
+}) 
